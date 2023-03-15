5f58d097d02780c2166de4b4863cc60e939616e9
Jehusu <jacobbjorklund2004@gmail.com>
2023-03-15 09:09:20 +0200

    Commit 7800

--- public/app.js
+++ public/app.js
@@ -1,13 +1,27 @@
 fetchapi()
 
-document.addEventListener('click', () => {
+document.querySelector('#search').addEventListener('click', () => {
     fetchapi()
 })
 
 async function fetchapi() {
     let input = document.querySelector('input').value
     const res = await fetch(`/api/pokemon?name=${input}`)
-    const data = await res.json()
+    let data = await res.json()
+
+    document.querySelector('#autist').addEventListener('click', () => {
+        data = data.sort((a, b) => b.name.localeCompare(a.name))
+        RenderTable(data)
+    })
+
+    document.querySelector('#Experience').addEventListener('click', () => {
+        let minWeight = document.querySelector('min-Weight')
+        let maxWeight = document.querySelector('max-Weight')
+        let data2 = data.filter(
+            pokemon => pokemon.weight >= minWeight && pokemon.weight <= maxWeight
+        )
+        RenderTable(data2)
+    })
 
     RenderTable(data)
     function RenderTable(json) {
